ecfd007918a1ffd1fd24812e007e3070807fcbcf
Paul Butler <butler.paul@gmail.com>
2022-09-29 18:25:32 +0000

    scripts and tasks updates

--- certs/edit-creds.sh
+++ certs/edit-creds.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-
-PATH=.;$PATH
-
-cert=`ls *-cert*`
-priv=`ls *-priv*`
+export PATH="$PWD:$PATH"
+#echo $PATH
+cert=`ls /home/studio/workspace/local/*-cert*`
+priv=`ls /home/studio/workspace/local/*-priv*`
 sed -z 's/\n/\\n/g;s/^/export CLIENT_CERTIFICATE_PEM=\"/;s/\(export CLIENT_CERT.*\)\\n$/\1\"\n/' $cert > cert.str
 sed -z 's/\n/\\n/g;s/^/export CLIENT_PRIVATE_KEY_PEM=\"/;s/\(export CLIENT_PRIV.*\)\\n$/\1\"\n/' $priv > priv.str
 cat cert.str >> secrets.txt
